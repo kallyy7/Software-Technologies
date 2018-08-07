7847c344e7e035cc74d68266a682d465cd669915
kallyy7 <milanova1203@gmail.com>
2018-08-07 15:33:46 +0300

    add

---
+++ Project Rider(exam) - C#/ProjectRider/Controllers/ProjectsController.cs
@@ -0,0 +1,96 @@
+﻿namespace ProjectRider.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ProjectRider.Models;
+    using System.Linq;
+
+    public class ProjectController : Controller
+    {
+        private readonly ProjectDbContext context;
+
+        public ProjectController(ProjectDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            // TODO
+            var projects = context.Projects.ToList();
+            return View(projects);
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            // TODO
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(Project project)
+        {
+            // TODO
+            context.Projects.Add(project);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            // TODO
+            var proj = context.Projects
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+
+            return View(proj);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int id, Project projectModel)
+        {
+            // TODO
+            
+            context.Projects.Update(projectModel);
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            // TODO
+            var proj = context.Projects
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+
+            return View(proj);
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id, Project projectModel)
+        {
+            // TODO
+            var proj = context
+                .Projects
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+
+            context.Projects.Remove(proj);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Project Rider(exam) - C#/ProjectRider/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectRider.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Project
+    {
+        // TODO
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public long Budget { get; set; }
+    }
+}

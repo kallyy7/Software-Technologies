da401af133bf748434552ce7a336112dc0a893a8
kallyy7 <milanova1203@gmail.com>
2018-08-07 17:09:37 +0300

    add CSHARP ASP.NET MVC OVERVIEW.NET MVC OVERVIEW - EXERCISES

---
+++ CSHARP ASP.NET MVC OVERVIEW.NET MVC OVERVIEW - EXERCISES/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            switch (calculator.Operator)
+            {
+                case "+":
+                    return calculator.LeftOperand + calculator.RightOperand;
+
+                case "-":
+                    return calculator.LeftOperand - calculator.RightOperand;
+
+                case "*":
+                    return calculator.LeftOperand * calculator.RightOperand;
+
+                case "/":
+                    return calculator.LeftOperand / calculator.RightOperand;
+
+                case "%":
+                    return calculator.LeftOperand % calculator.RightOperand;
+
+                case "^":
+
+                    var result = 1m;
+
+                    for (var index = 0; index < calculator.RightOperand; index++)
+                    {
+                        result *= calculator.LeftOperand;
+                    }
+
+                    return result;
+                default:
+                    return 0;
+            }
+
+        }
+    }
+}

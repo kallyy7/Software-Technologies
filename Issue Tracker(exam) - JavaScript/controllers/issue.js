92a7dc40e46ee5af8bc78ad6e6e018cc66b0f11e
Kalina Milanova <milanova1203@gmail.com>
2018-08-14 22:10:53 +0300

    Add files via upload

---
+++ Issue Tracker(exam) - JavaScript/controllers/issue.js
@@ -0,0 +1,50 @@
+const Issue = require('../models').Issue;
+
+module.exports = {
+    index: (req, res) => {
+        Issue.findAll().then(issues=>{
+                res.render('issue/index',{'issues': issues})});
+    },
+	
+    createGet: (req, res) => {
+        res.render('issue/create')
+    },
+	
+    createPost: (req, res) => {
+        let arg = req.body.issue;
+
+        Issue.create(arg).then(()=>{
+            return res.redirect("/");
+        })
+    },
+	
+    editGet: (req, res) => {
+        let id = req.params.id;
+        Issue.findById(id).then(issue=>{
+            res.render('issue/edit',{'issue':issue});
+        })
+    },
+
+    editPost: (req, res) => {
+        let id = req.params.id;
+        let args = req.body.issue;
+        Issue.findById(id).then(issue=>{
+            issue.updateAttributes(args).then(()=>res.redirect('/'));
+        })
+    },
+
+    deleteGet: (req, res) => {
+        let id =req.params.id;
+        Issue.findById(id).then(issue=>{
+            res.render('issue/delete',{'issue':issue});
+        })
+    },
+	
+    deletePost: (req, res) => {
+        let id =req.params.id;
+
+        Issue.findById(id).then(issue=>{
+            issue.destroy().then(()=>res.redirect('/'));
+        })
+    }
+};
---
+++ Issue Tracker(exam) - JavaScript/models/issue.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize) {
+
+        let Issue = sequelize.define('Issue', {
+            title: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: false,
+            },
+            priority: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            }
+        }, {
+            timestamps: false
+        });
+
+        return Issue;
+    };
